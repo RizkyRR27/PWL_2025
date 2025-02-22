d11294581adedc7a0047cbeb6b4432638ffaccdf
RizkyRR27 <rizkyroza2005@gmail.com>
2025-02-22 15:14:02 +0700

    js 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,63 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only([ 'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([ 'create', 'store', 'update', 'destroy'
+]);
+
+// Route::get('/', [PageController::class,'index']);
+// Route::get('/about', [PageController::class,'about']);
+// Route::get('/articles/{id}', [PageController::class,'articles']);
+// Route::get('/', HomeController::class);
+// Route::get('/about', AboutController::class);
+// Route::get('/articles/{id}', ArticleController::class);
+
+
+
+//Route::get('/hello', [WelcomeController::class,'hello']);
+
+//  Route::get('/user/{name?}', function ($name=null) { 
+//      return 'Nama saya '.$name;
+// });
+// Route::get('/user/{name?}', function ($name="john") { 
+//     return 'Nama saya '.$name;
+// });
+
+
+// Route::get('/articles/{id} ', function ($id) {
+//     return 'Halaman Artikel dengan ID'. $id;
+//     });
+
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+//     });
+    
+// Route::get('/about', function () { 
+//     return 'Rizky Roza 2341760150';
+// });
+
+// Route::get('/', function () { 
+//     return 'selamat datang';
+// });
+
+// Route::get('/world', function () {
+// return 'World';
+// });
+
+// Route::get('/hello', function () { 
+//     return 'Hello World';
+// });
+
+
 use App\Http\Controllers\ItemController;
 
 /*
@@ -16,8 +73,13 @@ use App\Http\Controllers\ItemController;
 
 
 #cara untuk mendefinisikan rute 'item' untuk controller itemController
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::resource('items', ItemController::class);
+// Route::get('/user/{name}', function ($name) { 
+//     return 'Nama saya '.$name;
+// });
+
+
+//Route::resource('items', ItemController::class);
